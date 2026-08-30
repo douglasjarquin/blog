55c4d9b062e711eddfdae3f4cd1b5447f4b43b1c
Douglas Jarquin <8209+douglasjarquin@users.noreply.github.com>
2026-08-29 20:54:25 -0400

    no-mistakes(lint): Format og-image template with prettier

--- src/pages/og-image/[...slug].png.ts
+++ src/pages/og-image/[...slug].png.ts
@@ -30,31 +30,35 @@ const ogOptions: SatoriOptions = {
 
 const markup = (title: string, pubDate: string) =>
 	html`<div tw="flex flex-col w-full h-full bg-[#f8f4ec] text-[#282218]">
-			<div tw="flex flex-1 items-center w-full px-16">
-				<div tw="flex items-center justify-center w-72 h-72 rounded-full border-8 border-[#3f704d] bg-[#3f704d] text-[#f8f4ec]">
-					<p tw="text-8xl font-bold italic">DJ</p>
-				</div>
-				<div tw="flex flex-col flex-1 ml-14">
-					<p tw="text-2xl mb-6 uppercase tracking-widest text-[#8d8371]">${pubDate}</p>
-					<h1 tw="text-6xl font-bold leading-snug">${title}</h1>
-				</div>
+		<div tw="flex flex-1 items-center w-full px-16">
+			<div
+				tw="flex items-center justify-center w-72 h-72 rounded-full border-8 border-[#3f704d] bg-[#3f704d] text-[#f8f4ec]"
+			>
+				<p tw="text-8xl font-bold italic">DJ</p>
 			</div>
-			<div tw="flex items-center justify-between w-full px-16 py-8 border-t-4 border-[#3f704d] text-xl">
-				<div tw="flex items-center">
-						<div tw="flex w-5 h-5 mr-2 border-2 border-[#3f704d] bg-[#3f704d]"></div>
-						<div tw="flex w-5 h-5 mr-2 border-2 border-[#3f704d]"></div>
-						<div tw="flex w-5 h-5 mr-2 border-2 border-[#3f704d] bg-[#3f704d]"></div>
-						<div tw="flex w-5 h-5 mr-2 border-2 border-[#3f704d]"></div>
-						<div tw="flex w-5 h-5 mr-2 border-2 border-[#3f704d] bg-[#3f704d]"></div>
-						<div tw="flex w-5 h-5 mr-2 border-2 border-[#3f704d]"></div>
-						<div tw="flex w-5 h-5 mr-2 border-2 border-[#3f704d] bg-[#3f704d]"></div>
-						<div tw="flex w-5 h-5 mr-2 border-2 border-[#3f704d]"></div>
-						<div tw="flex w-5 h-5 mr-2 border-2 border-[#3f704d] bg-[#3f704d]"></div>
-						<div tw="flex w-5 h-5 mr-2 border-2 border-[#3f704d]"></div>
-					</div>
-				<p tw="font-semibold">by ${siteConfig.author}</p>
+			<div tw="flex flex-col flex-1 ml-14">
+				<p tw="text-2xl mb-6 uppercase tracking-widest text-[#8d8371]">${pubDate}</p>
+				<h1 tw="text-6xl font-bold leading-snug">${title}</h1>
 			</div>
-		</div>`;
+		</div>
+		<div
+			tw="flex items-center justify-between w-full px-16 py-8 border-t-4 border-[#3f704d] text-xl"
+		>
+			<div tw="flex items-center">
+				<div tw="flex w-5 h-5 mr-2 border-2 border-[#3f704d] bg-[#3f704d]"></div>
+				<div tw="flex w-5 h-5 mr-2 border-2 border-[#3f704d]"></div>
+				<div tw="flex w-5 h-5 mr-2 border-2 border-[#3f704d] bg-[#3f704d]"></div>
+				<div tw="flex w-5 h-5 mr-2 border-2 border-[#3f704d]"></div>
+				<div tw="flex w-5 h-5 mr-2 border-2 border-[#3f704d] bg-[#3f704d]"></div>
+				<div tw="flex w-5 h-5 mr-2 border-2 border-[#3f704d]"></div>
+				<div tw="flex w-5 h-5 mr-2 border-2 border-[#3f704d] bg-[#3f704d]"></div>
+				<div tw="flex w-5 h-5 mr-2 border-2 border-[#3f704d]"></div>
+				<div tw="flex w-5 h-5 mr-2 border-2 border-[#3f704d] bg-[#3f704d]"></div>
+				<div tw="flex w-5 h-5 mr-2 border-2 border-[#3f704d]"></div>
+			</div>
+			<p tw="font-semibold">by ${siteConfig.author}</p>
+		</div>
+	</div>`;
 
 type Props = InferGetStaticPropsType<typeof getStaticPaths>;
 
