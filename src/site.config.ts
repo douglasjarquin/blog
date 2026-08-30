2510739d069088810194ddc73d1d640ebf4e4100
Douglas Jarquin <8209+douglasjarquin@users.noreply.github.com>
2026-08-29 20:44:02 -0400

    Replace starter OG image and description

--- src/site.config.ts
+++ src/site.config.ts
@@ -14,7 +14,8 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	// Used as the default description meta property and webmanifest description
-	description: "Cozy up to this corner of the internet with Douglas Jarquin and his thoughts.",
+	description:
+		"I'm Doug and this is my site. I write about web development, programming, and other things I'm interested in.",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
 	lang: "en-US",
 	// Meta property, found in src/components/BaseHead.astro L:42
